558dbc5094b901fbb741d97d7809095ee77b09d9
LeeYongKyu <knsesang2000@gmail.com>
2020-09-23 21:16:28 +0900

    #001. 기본 폴더 및 파일 생성

---
+++ appRooms/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+from django.
+class ApproomsConfig(AppConfig):
+    name = 'appRooms'
---
+++ appUsers/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+from django
+# Create your models here.
+class clsUser(AbstractUser):
+   pass
+
+
+   
