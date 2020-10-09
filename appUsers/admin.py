b5e4204782fb4ae722c048c945f7132f1721e2e2
LeeYongKyu <knsesang2000@gmail.com>
2020-10-10 04:58:06 +0900

    3-5. [ADD] UserAdmin + CustomAdmin

--- appUsers/admin.py
+++ appUsers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
@@ -7,7 +8,25 @@ from . import models
 # admin.site.register(models.clsUser, clsCustomUserAdmin) = @admin.register(models.clsUser) 같은 표현 방식
 #   어드민 패널에서 models.clsUser 를 보고싶다는 의미ㄴ
 @admin.register(models.clsUser)
-class clsCustomUserAdmin(admin.ModelAdmin):
+#   class clsCustomUserAdmin(admin.ModelAdmin):
+class clsCustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "custom Profile",
+            {
+                "fields": (
+                    "varAvatar",
+                    "varGender",
+                    "varBio",
+                    "varBirthdate",
+                    "varLanguage",
+                    "varCurrency",
+                    "varSuperhost",
+                )
+            },
+        ),
+    )
 
     #   email 이메일은 장고 기본 모델에서 불러옴
     list_display = (
