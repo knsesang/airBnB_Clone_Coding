70e8dcea0a1f904f4a970bd6b21a31fd8308e5a3
LeeYongKyu <knsesang2000@gmail.com>
2020-10-12 04:42:35 +0900

    10-1 [ADD] HttpResponse and render

--- appRooms/views.py
+++ appRooms/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import datetime
 
 # Create your views here.
 def fn_All_Rooms(request):
-    pass
+    #   print(request)
+    #   <WSGIRequest: GET '/'>
+
+    #   print(vars(request))
+    #   {'environ': {'HOSTNAME': 'docker_aairBnB', 'SHLVL': '1', ....
+
+    #   print(dir(request))
+    #   ['COOKIES', 'FILES', 'GET', 'META', 'POST', ...
+    now = datetime.now()
+
+    return HttpResponse(content=f"{now} hello")
